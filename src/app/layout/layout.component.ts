718aa107723ee3cc21a8bc81094c4dc4ea054cff
liza_matsenka <macenko1996@gmail.com>
2022-11-13 15:03:06 +0300

    added products section

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,8 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+  products = [
+    {
+      id: 1,
+      img: './assets/img/pizza_1.png',
+      title: 'Мясная Делюкс',
+      description: 'Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы'
+    },
+    {
+      id: 2,
+      img: './assets/img/pizza_2.png',
+      title: 'Морская Премиум',
+      description: 'Перец, сыр, креветки, кальмары, мидии, лосось'
+    },
+    {
+      id: 3,
+      img: './assets/img/pizza_3.png',
+      title: 'Бекон и Сосиски',
+      description: 'Бекон, сыр, сосиски, ананас, томатная паста'
+    },
+    {
+      id: 4,
+      img: './assets/img/pizza_4.png',
+      title: 'Куриная Делюкс',
+      description: 'Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста'
+    },
+    {
+      id: 5,
+      img: './assets/img/pizza_5.png',
+      title: 'Барбекю Премиум',
+      description: 'Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили'
+    },
+    {
+      id: 6,
+      img: './assets/img/pizza_6.png',
+      title: 'Пепперони Дабл',
+      description: 'Пепперони, сыр, колбаса 2 видов: обжаренная и вареная'
+    },
+    {
+      id: 7,
+      img: './assets/img/pizza_7.png',
+      title: 'Куриное трио',
+      description: 'Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы'
+    },
+    {
+      id: 8,
+      img: './assets/img/pizza_8.png',
+      title: 'Сырная',
+      description: 'Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный'
+    },
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
